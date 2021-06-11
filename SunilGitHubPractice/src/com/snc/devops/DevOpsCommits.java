98717f8f3fd90e7d18949b75a52ac4967a556e98
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2021-06-11 15:41:08 +0530

    STRY0010009: Git Commits Import and mapping to work item

---
+++ SunilGitHubPractice/src/com/snc/devops/DevOpsCommits.java
@@ -0,0 +1,12 @@
+package com.snc.devops;
+
+public class DevOpsCommits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git Commits Import and mapping to work item: STRY0010009");
+		
+	}
+
+}
