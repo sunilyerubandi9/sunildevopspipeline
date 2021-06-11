72d4c14e95f6900981e110bd4c2d4a70f27ce892
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2021-06-11 16:22:47 +0530

    STRY0010009: updating existing module

--- SunilGitHubPractice/src/com/snc/devops/DevOpsCommits.java
+++ SunilGitHubPractice/src/com/snc/devops/DevOpsCommits.java
@@ -7,6 +7,9 @@ public class DevOpsCommits {
 		
 		System.out.println("Git Commits Import and mapping to work item: STRY0010009");
 		
+		
+		System.out.println("Git Commit: 4 for STRY0010009 adding updating existing module");
+		
 	}
 
 }
