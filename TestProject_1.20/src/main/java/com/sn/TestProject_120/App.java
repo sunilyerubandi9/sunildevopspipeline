103c49b731609cf4ff07481a08810a48afefff1f
Admin McAdmin <test@dummy.com>
2021-07-02 09:05:18 +0530

    724: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -1,4 +1,4 @@
-package com.sn.TestProject_120;
+	package com.sn.TestProject_120;
 
 /**
  * Hello world!
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!, making chages to test work item. Changes- PipelineUI" );
+        
+        System.out.println("Commit 1 made to Test Project 001" );
     }
 }
