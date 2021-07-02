4e135299fa11a8ee606d42c3b29d86ad2cc7e683
Admin McAdmin <test@dummy.com>
2021-07-02 07:49:13 +0530

    New Maven test project is adding to the Repo

---
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -0,0 +1,13 @@
+package com.sn.TestProject_120;
+
+/**
+ * Hello world!
+ * new changes
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, making chages to test work item. Changes- PipelineUI" );
+    }
+}
