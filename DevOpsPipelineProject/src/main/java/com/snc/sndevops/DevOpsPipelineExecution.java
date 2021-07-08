da6b771d7c4d5e93fcaae63cc5ef72a6486044f2
Admin McAdmin <test@dummy.com>
2021-07-08 20:40:31 +0530

    STRY0010086: Story update

--- DevOpsPipelineProject/src/main/java/com/snc/sndevops/DevOpsPipelineExecution.java
+++ DevOpsPipelineProject/src/main/java/com/snc/sndevops/DevOpsPipelineExecution.java
@@ -163,7 +163,9 @@ public class DevOpsPipelineExecution {
 		System.out.println("STRY0010055: Story update");
 		
 		System.out.println("STRY0010084: Story update");
+
 		
+		System.out.println("STRY0010086: Story update");
 		
 	
 
