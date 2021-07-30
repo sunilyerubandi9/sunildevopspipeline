3d287edb92c8a0c2fdb46f0ad51724aa57ed2f28
Admin McAdmin <test@dummy.com>
2021-07-30 07:46:01 +0530

    STRY0010007: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("STRY0010001: Story update");
+        System.out.println("STRY0010007: Story update");
    
     }
 }
