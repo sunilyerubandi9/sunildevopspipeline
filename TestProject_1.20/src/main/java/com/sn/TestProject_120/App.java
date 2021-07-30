aea3816ef377f2d8a9d33fc0fdc733557a8b67e8
Admin McAdmin <test@dummy.com>
2021-07-30 07:37:03 +0530

    STRY0010001: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -4,18 +4,10 @@
  * Hello world!
  * new changes
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!, making chages to test work item. Changes- PipelineUI" );
+public class App {
+    public static void main( String[] args ) {
         
-        System.out.println("Commit 1 made to Test Project 001" );
-        
-        System.out.println("Commit 2 added new story seismic changes" );
-        
-        System.out.println("Commit 3 added new story seismic changes" );
-        
-        System.out.println("Commit 6 added new story seismic changes" );
+        System.out.println("STRY0010001: Story update");
+   
     }
 }
