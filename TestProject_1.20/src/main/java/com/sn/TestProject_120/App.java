1fe1da67b74eba8cebb243d2e47918207f3ce850
Admin McAdmin <test@dummy.com>
2021-08-13 00:42:26 +0530

    STRY0010099: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("STRY0010165: Story update");
+        System.out.println("STRY0010099: Story update");
    
     }
 }
