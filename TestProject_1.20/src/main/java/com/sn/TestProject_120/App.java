a1827ece5acec8aaa0c5dbe8c878085b8e986279
Admin McAdmin <test@dummy.com>
2021-08-15 08:45:47 +0530

    749: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("754: Story update");
+        System.out.println("749: Story update");
    
     }
 }
