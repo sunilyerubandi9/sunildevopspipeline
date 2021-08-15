116801752c04dbfbefe5b6c26fe314b2103fa58e
Admin McAdmin <test@dummy.com>
2021-08-15 22:08:07 +0530

    765: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("764: Story update ");
+        System.out.println("765: Story update ");
    
     }
 }
