95b613985633f4fc6e35e384e5f80ebef8c1e15a
Admin McAdmin <test@dummy.com>
2021-08-15 19:57:38 +0530

    768: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("766: Story update");
+        System.out.println("768: Story update");
    
     }
 }
