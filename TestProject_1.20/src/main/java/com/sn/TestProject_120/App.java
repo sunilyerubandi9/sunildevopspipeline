b123a516be48696859475dd2d4629bf69848bac0
Admin McAdmin <test@dummy.com>
2021-08-15 08:48:39 +0530

    749: Bug update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("749: Story update");
+        System.out.println("749: Bug update");
    
     }
 }
