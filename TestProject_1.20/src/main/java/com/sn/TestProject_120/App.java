094150b0af6e41b0f0599d8f493c806482bb97ba
Admin McAdmin <test@dummy.com>
2021-08-19 19:36:49 +0530

    742: Story update 742

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@ package com.sn.TestProject_120;
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("741: Story update 741");
+        System.out.println("742: Story update 742");
    
     }
 }
