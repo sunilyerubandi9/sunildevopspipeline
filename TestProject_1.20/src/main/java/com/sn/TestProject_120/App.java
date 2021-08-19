05aa16958358add544e876e8326c5c724412bb3b
Admin McAdmin <test@dummy.com>
2021-08-19 17:51:48 +0530

    735: Story update 735

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@ package com.sn.TestProject_120;
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("734: Story update 734");
+        System.out.println("735: Story update 735");
    
     }
 }
