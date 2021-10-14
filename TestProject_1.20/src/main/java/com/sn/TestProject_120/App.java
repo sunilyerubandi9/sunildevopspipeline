b8cdccd1ecea198321240db82a036e2a720ca398
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2021-10-14 23:42:55 +0530

    STRY0010102: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@ package com.sn.TestProject_120;
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("STRY0010085: Story update");
+        System.out.println("STRY0010102: Story update");
    
     }
 }
