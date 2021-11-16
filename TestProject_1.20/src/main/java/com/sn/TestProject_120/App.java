150314217ba0148909337db35c49d41dc9e89a97
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-16 21:33:42 +0530

    737: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@ package com.sn.TestProject_120;
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("746: Story update");
+        System.out.println("737: Story update");
    
     }
 }
