ab72440f71e733f8be7e5c1e4e605f2d006edf9b
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-16 17:07:08 +0530

    719: story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,11 +7,7 @@ package com.sn.TestProject_120;
 public class App {
     public static void main( String[] args ) {
         
-<<<<<<< HEAD
-        System.out.println("STRY0010132: Story update");
-=======
         System.out.println("719: Story update");
->>>>>>> ea29110... 719: Story update
    
     }
 }
