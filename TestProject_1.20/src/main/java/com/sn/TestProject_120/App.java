bf6a796bdf1712c301d71fb06c06238956889429
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-18 17:30:59 +0530

    726: Story update

--- TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -7,7 +7,7 @@ package com.sn.TestProject_120;
 public class App {
     public static void main( String[] args ) {
         
-        System.out.println("725: Story update");
+        System.out.println("726: Story update");
    
     }
 }
