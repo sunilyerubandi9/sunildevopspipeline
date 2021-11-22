5d6f70a80d8aefb31917e03090892ac8872f363b
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-22 12:19:08 +0530

    785: Story Update

--- DevOpsPipelineProject/src/main/java/com/snc/sndevops/DevOpsPipelineExecution.java
+++ DevOpsPipelineProject/src/main/java/com/snc/sndevops/DevOpsPipelineExecution.java
@@ -5,50 +5,7 @@ public class DevOpsPipelineExecution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Git Commits Import and mapping to work item: STRY0010009");
-		
-		System.out.println("Modeling Jenkins Pipeline with ServiceNow Devops with Chagne Acceleration");
-		
-		System.out.println("jenkinsreceives notifications from GitHub");
-		
-		System.out.println("jenkinsreceives notifications from GitHub by adding repo & pipeline to App");
-		
-		System.out.println("jenkinsreceives notifications from GitHub by adding repo & pipeline to App22");
-		
-		
-		System.out.println("jenkinsreceives notifications from GitHub by adding repo & pipeline to App22");
-		
-		System.out.println("jenkinsreceives notifications from GitHub by adding repo & pipeline to App333");
-		
-		System.out.println("jenkinsreceives notifications from GitHub by adding repo & pipeline to App4444");
-		
-		System.out.println("STRY0010084");
-		
-		System.out.println("STRY0010084");
-		
-		System.out.println("STRY0010007");
-		
-		System.out.println("STRY0010009");
-		
-		System.out.println("STRY0010010");
-		
-		System.out.println("STRY0010019");
-		
-		System.out.println("STRY0010027");
-		
-		System.out.println("STRY0010028");
-		
-		System.out.println("STRY0010031");
-		
-		System.out.println("STRY0010032");
-		
-		System.out.println("STRY0010033");
-		
-		System.out.println("STRY0010038");
-		
-		System.out.println("STRY0010046");
-		
-		System.out.println("STRY0010084");
+		System.out.println("785: Story Update");
 		
 	}
 
