c984fcfcc578b89b2964013e0688e3101e67c8a8
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-22 17:01:58 +0530

    789: Story Updated

--- DevOpsPipelineProject/src/main/java/com/snc/sndevops/DevOpsPipelineExecution.java
+++ DevOpsPipelineProject/src/main/java/com/snc/sndevops/DevOpsPipelineExecution.java
@@ -5,7 +5,7 @@ public class DevOpsPipelineExecution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("788: Story Updated");
+		System.out.println("789: Story Updated");
 		
 	}
 
